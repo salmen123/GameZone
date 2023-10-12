915f4149f57761d14e2fda8f64a6b810a3416b93
Mohamed Salmen Saadi <salmen.saadi@cgi.com>
2023-10-12 09:35:51 -0400

    Add EF and DbContext

--- GameZone/Program.cs
+++ GameZone/Program.cs
@@ -1,6 +1,15 @@
+using GameZone.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                                   ?? throw new InvalidOperationException("No connection string was found");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
