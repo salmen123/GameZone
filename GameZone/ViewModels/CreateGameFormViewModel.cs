5f20360f4b55245abd063df151f9793636fc2212
Mohamed Salmen Saadi <salmen.saadi@cgi.com>
2023-10-19 08:22:35 -0400

    Add Form ViewModel

---
+++ GameZone/ViewModels/CreateGameFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.ViewModels
+{
+    public class CreateGameFormViewModel
+    {
+        [MaxLength(250)]
+        public string Name { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        public List<int> SelectedDevices { get; set; } = new List<int>();
+
+        public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public IFormFile Cover { get; set; } = default!;
+    }
+}
