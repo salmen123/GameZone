3b784fb2d67999a972a8b572357832a73aa8cdcc
Mohamed Salmen Saadi <salmen.saadi@cgi.com>
2023-10-19 10:22:45 -0400

    Add Form Inputs

--- GameZone/Controllers/GamesController.cs
+++ GameZone/Controllers/GamesController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameZone.Data;
+using GameZone.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameZone.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,7 +22,18 @@ namespace GameZone.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            CreateGameFormViewModel viewModel = new()
+            {
+                Categories = _context.Categories
+                    .Select(c => new SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                    .OrderBy(c => c.Text)
+                    .ToList(),
+                Devices = _context.Devices
+                    .Select(d => new SelectListItem { Value = d.Id.ToString(), Text = d.Name })
+                    .OrderBy(d => d.Text)
+                    .ToList()
+            };
+            return View(viewModel);
         }
     }
 }
--- GameZone/ViewModels/CreateGameFormViewModel.cs
+++ GameZone/ViewModels/CreateGameFormViewModel.cs
@@ -8,10 +8,12 @@ namespace GameZone.ViewModels
         [MaxLength(250)]
         public string Name { get; set; } = string.Empty;
 
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
 
+        [Display(Name = "Supported Devices")]
         public List<int> SelectedDevices { get; set; } = new List<int>();
 
         public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
